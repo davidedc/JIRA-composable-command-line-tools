126ff94e1aa692dc74cd57647cf2ec6e7c0a5dcc
Davide Della Casa <davidedc@gmail.com>
2016-02-24 10:56:56 +0000

    cleaning up a little allTheWorkNeeded.py and making sure there are no duplicates in the final list

--- allTheWorkNeeded.py
+++ allTheWorkNeeded.py
@@ -1,5 +1,7 @@
-# This script shows how to connect to a JIRA instance with a
-# username and password over HTTP BASIC authentication.
+# This script "closes" an initial list of issues under
+# the operations of "subtasks, bloked by, requires, linked issues to this epic"
+# it's not a tree because the links can point to any issue, so it's
+# a proper graph visit.
 
 from jira.client import JIRA
 import csv
@@ -8,14 +10,20 @@ import time
 import configurationData
 
 reader = csv.reader(sys.stdin)
-processedIssues = {}
-issuesToBeProcessed = {}
-       
-# By default, the client will connect to a JIRA instance started from the Atlassian Plugin SDK.
-# See
-# https://developer.atlassian.com/display/DOCS/Installing+the+Atlassian+Plugin+SDK
-# for details.
 
+# a processed issue is an issue where all the
+# "subtasks, bloked by, requires, linked issues to this epic"
+# have been output in the list. Note that these outputted issues
+# might not be "processedIssues" yet. This list only grows.
+processedIssues = {}
+
+# queue of issues to be processed. This list grows and shrinks.
+issuesToBeProcessed = {}
+
+# all issues that make to the list end up here. Not all these
+# issues might be processedIssues yet. This list only grows.
+listedIssues = {}
+       
 options = {
     'server': configurationData.jiraServerUrl
 }
@@ -37,14 +45,17 @@ for row in csv.reader(iter(sys.stdin.readline, '')):
 	rowNumber = rowNumber + 1
 	# skip header
 	if rowNumber > 1:
-		issuesToBeProcessed[row[0]] = row
 
 		key = row[0]
 		# first, get the very issue itself. It's a query that will return only one issue.
 		issues = jira.search_issues('id in (' + key + ')')
 		for eachIssue in issues:
-			writer.writerow({'issue id': eachIssue.key, 'why listed': 'in starting list: ' + key, 'summary': eachIssue.fields.summary})
-			sys.stdout.flush()
+			if (not eachIssue.key in processedIssues) and (not eachIssue.key in listedIssues):
+				issuesToBeProcessed[eachIssue.key] = 1
+				listedIssues[eachIssue.key] = 1
+				listedIssues[eachIssue.key] = 1
+				writer.writerow({'issue id': eachIssue.key, 'why listed': 'in starting list: ' + key, 'summary': eachIssue.fields.summary})
+				sys.stdout.flush()
 
 
 while issuesToBeProcessed:
@@ -60,40 +71,40 @@ while issuesToBeProcessed:
 	issues = jira.search_issues('parent in (' + key + ')')
 	for eachIssue in issues:
 		#print ' > '+ eachIssue
-		if eachIssue.key in processedIssues:
-			continue
-		issuesToBeProcessed[''+eachIssue.key] = 1
-		writer.writerow({'issue id': eachIssue.key, 'why listed': 'child of: ' + key, 'summary': eachIssue.fields.summary})
-		sys.stdout.flush()
+		if (not eachIssue.key in processedIssues) and (not eachIssue.key in listedIssues):
+			issuesToBeProcessed[eachIssue.key] = 1
+			listedIssues[eachIssue.key] = 1
+			writer.writerow({'issue id': eachIssue.key, 'why listed': 'child of: ' + key, 'summary': eachIssue.fields.summary})
+			sys.stdout.flush()
 
 	issues = jira.search_issues('"Epic Link" in (' + key + ')')
 	for eachIssue in issues:
 		#print ' > '+ eachIssue
-		if eachIssue.key in processedIssues:
-			continue
-		issuesToBeProcessed[''+eachIssue.key] = 1
-		writer.writerow({'issue id': eachIssue.key, 'why listed': 'linked to epic: ' + key, 'summary': eachIssue.fields.summary})
-		sys.stdout.flush()
+		if (not eachIssue.key in processedIssues) and (not eachIssue.key in listedIssues):
+			issuesToBeProcessed[eachIssue.key] = 1
+			listedIssues[eachIssue.key] = 1
+			writer.writerow({'issue id': eachIssue.key, 'why listed': 'linked to epic: ' + key, 'summary': eachIssue.fields.summary})
+			sys.stdout.flush()
 
 	issues = jira.search_issues('issue in linkedIssues(' + key + ',"requires")')
 	for eachIssue in issues:
 		#print ' > '+ eachIssue
-		if eachIssue.key in processedIssues:
-			continue
-		issuesToBeProcessed[''+eachIssue.key] = 1
-		writer.writerow({'issue id': eachIssue.key, 'why listed': 'required by: ' + key, 'summary': eachIssue.fields.summary})
-		sys.stdout.flush()
+		if (not eachIssue.key in processedIssues) and (not eachIssue.key in listedIssues):
+			issuesToBeProcessed[eachIssue.key] = 1
+			listedIssues[eachIssue.key] = 1
+			writer.writerow({'issue id': eachIssue.key, 'why listed': 'required by: ' + key, 'summary': eachIssue.fields.summary})
+			sys.stdout.flush()
 
 	issues = jira.search_issues('issue in linkedIssues(' + key + ',"is blocked by")')
 	for eachIssue in issues:
 		#print ' > '+ eachIssue
-		if eachIssue.key in processedIssues:
-			continue
-		issuesToBeProcessed[''+eachIssue.key] = 1
-		writer.writerow({'issue id': eachIssue.key, 'why listed': 'blocks: ' + key, 'summary': eachIssue.fields.summary})
-		sys.stdout.flush()
+		if (not eachIssue.key in processedIssues) and (not eachIssue.key in listedIssues):
+			issuesToBeProcessed[eachIssue.key] = 1
+			listedIssues[eachIssue.key] = 1
+			writer.writerow({'issue id': eachIssue.key, 'why listed': 'blocks: ' + key, 'summary': eachIssue.fields.summary})
+			sys.stdout.flush()
 
 
-	processedIssues[''+key] = 1
-	if ''+key in issuesToBeProcessed:
-		del issuesToBeProcessed[''+key]
+	processedIssues[key] = 1
+	if key in issuesToBeProcessed:
+		del issuesToBeProcessed[key]
